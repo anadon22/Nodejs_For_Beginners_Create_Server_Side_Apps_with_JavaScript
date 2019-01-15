b26083f036891098bbee09b63c91d0f76bf87cff
anadon22 <46505967+anadon22@users.noreply.github.com>
2019-01-15 22:22:44 +0100

    to be continues

--- hello.js
+++ hello.js
@@ -3,12 +3,15 @@ var moment = require('moment');
 
 
 function serverCallback (req, res) {
+  var startTime = "10:00";
+  var endTime = "12:00";
+  var currentTime = moment().format('LTS');
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  // res.end("Hello world" + process.argv[2]);
-  // full format of time and date
-  // res.end("Hello" + " it is " + moment().format());
-  // better format of time and date
-  res.end("Hello" + " it is " + moment().format('LLLL'));
+	res.end("Hello Zenva!\
+    Welcome to our page.\
+    Now, it is "+ currentTime+".\
+    Our business hours is from " + startTime + " to " + endTime + ".\
+    Please come back in "+ moment(startTime).diff(currentTime, 'minutes')+".");
 
 }
 http.createServer(serverCallback).listen(8080);
