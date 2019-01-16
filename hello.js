b20356582a355b4567ace6cd0610375dafc5c971
anadon22 <46505967+anadon22@users.noreply.github.com>
2019-01-16 22:11:25 +0100

    to be continued part 2

--- hello.js
+++ hello.js
@@ -5,13 +5,21 @@ var moment = require('moment');
 function serverCallback (req, res) {
   var startTime = "10:00";
   var endTime = "12:00";
-  var currentTime = moment().format('LTS');
+  var currentTime = moment().format('HH:mm');
+  // var diff = moment(endTime.diff(startTime, "minutes"), "mm")
+
+  var msg = "Hello" + process.argv[2] + "!\n";
+  msg += "Welcome to out page.\n";
+  msg += "Now, it is " + currentTime + ".\n";
+  msg += "Our business hours is from " + startTime + " to " + endTime + ".\n";
+  msg += "Please come back in ";
+
+
+
+  // var difference = startTime.diff(currentTime, 'minutes');
+
   res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end("Hello Zenva!\
-    Welcome to our page.\
-    Now, it is "+ currentTime+".\
-    Our business hours is from " + startTime + " to " + endTime + ".\
-    Please come back in "+ moment(startTime).diff(currentTime, 'minutes')+".");
+	res.end(msg);
 
 }
 http.createServer(serverCallback).listen(8080);
